b26f293a6b35f49a096ff102d54e7cff18853d7c
valengratcd <lamela.valen.et.32@gmail.com>
2025-07-06 21:24:57 -0300

    todo mejorado

--- ARRAYS-UNIDIMENSIONALES-VECTORES-y-LISTAS/estrictamenteCreciente.py
+++ ARRAYS-UNIDIMENSIONALES-VECTORES-y-LISTAS/estrictamenteCreciente.py
@@ -1,15 +1,14 @@
 from typing import List
 
 def estrictamenteCreciente(arr: List) -> int:
-    orden = arr[1] - arr[0]
     rompe_orden = 0
     for i in range(1,len(arr[1:])):
-        if (arr[i + 1] - arr[i]) != orden:
+        if arr[i] >= arr[i + 1]:
             rompe_orden += 1
     return rompe_orden
 
 def main():
-    arreglo1 = [1,4,4,6]
+    arreglo1 = [1,4,4,6,4]
     print(estrictamenteCreciente(arreglo1))
 
 if __name__ == "__main__":
